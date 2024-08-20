4d1d11a66b6961f3e2c2539986be002d8c7447e8
Jameson Liu <jameson2170@gmail.com>
2024-08-20 01:53:43 -0700

    more usable input

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import './App.css';
 import Pendulum from './Pendulum.js'
 
 function App() {
-  const [target, setTarget] =  useState(0);
-
   // https://codepen.io/rafaelcastrocouto/pen/NWajBgM
   // https://blog.stackademic.com/matter-js-with-react-and-learn-to-use-useref-effectively-8f57365b926e
+  const [inputValue, setInputValue] = useState(0);
+  const [target, setTarget] = useState(0);
 
   return (
     <div style={{ width: 1000, height: 1000 }}>
       <input
         type="number"
-        value={target}
-        onChange={(e) => setTarget(parseFloat(e.target.value))}
+        defaultValue={0}
+        onChange={(e) => setInputValue(e.target.value)}
       />
-      <button onClick={() => {
-        
-      }}>reset</button>
+      <button onClick={() => {if (inputValue !== "") setTarget(inputValue)}} >reset</button>
       <Pendulum kP={0.7} kI={0} kD={2} target={target} />
     </div>
   );
